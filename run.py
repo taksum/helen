712b73a271d2c4bda35b62ca94307407a0134771
Chan Tak Sum <chantaksum1999@gmail.com>
2020-08-15 23:43:41 +0800

    headless

--- run.py
+++ run.py
@@ -10,6 +10,16 @@ driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 driver.get('https://sisprod.psft.ust.hk/psp/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?pslnkid=Z_HC_SSS_STUDENT_CENTER_LNK&FolderPath=PORTAL_ROOT_OBJECT.Z_HC_SSS_STUDENT_CENTER_LNK&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder')
 WebDriverWait(driver, 60).until(EC.title_contains("Student Center"))
 
+cookies = driver.get_cookies()
+driver.quit()
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=options)
+driver.get('https://sisprod.psft.ust.hk/psc/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ExactKeys=Y&TargetFrameName=None')
+for cookie in cookies:
+    driver.add_cookie(cookie)
 driver.get('https://sisprod.psft.ust.hk/psc/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ExactKeys=Y&TargetFrameName=None')
 driver.find_element_by_link_text('Plan').click()
 
