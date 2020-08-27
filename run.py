3fa82ede3021c737341c2f534ced77e153daab0f
Chan Tak Sum <chantaksum1999@gmail.com>
2020-08-27 17:59:07 +0800

    updated to chrome version 85

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ from datetime import datetime, time
 TIME = '00:00'
 
 webdriver_path = {
-    'Linux': './chrome_driver/chromedriver_linux',
-    'Darwin': './chrome_driver/chromedriver_mac',
+    'Linux'  : './chrome_driver/chromedriver_linux',
+    'Darwin' : './chrome_driver/chromedriver_mac',
     'Windows': './chrome_driver/chromedriver.exe'
 }
 
@@ -22,8 +22,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 # login logic
-driver = webdriver.Chrome(
-    executable_path=webdriver_path[system], options=options)
+driver = webdriver.Chrome(executable_path=webdriver_path[system], options=options)
 driver.get('https://sisprod.psft.ust.hk/psp/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?pslnkid=Z_HC_SSS_STUDENT_CENTER_LNK&FolderPath=PORTAL_ROOT_OBJECT.Z_HC_SSS_STUDENT_CENTER_LNK&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder')
 WebDriverWait(driver, 120).until(EC.title_contains("Student Center"))
 
@@ -31,8 +30,7 @@ WebDriverWait(driver, 120).until(EC.title_contains("Student Center"))
 driver.get('https://sisprod.psft.ust.hk/psc/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ExactKeys=Y&TargetFrameName=None')
 driver.find_element_by_link_text('Plan').click()
 
-payload = driver.find_element_by_id(
-    'SSR_REGFORM_VW$scroll$0').get_attribute('innerHTML').count('P_SELECT')
+payload = driver.find_element_by_id('SSR_REGFORM_VW$scroll$0').get_attribute('innerHTML').count('P_SELECT')
 
 if not payload:
     sys.exit('Nothing is in your shopping cart.')
@@ -46,8 +44,7 @@ startTime = time(*(map(int, TIME.split(':'))))
 WebDriverWait(driver, 1000, 0.002).until(lambda s: datetime.today().time() > startTime)
 button.click()
 
-WebDriverWait(driver, 300, 0.002).until(EC.presence_of_element_located(
-    (By.ID, 'DERIVED_REGFRM1_SSR_PB_SUBMIT')))
+WebDriverWait(driver, 300, 0.002).until(EC.presence_of_element_located((By.ID, 'DERIVED_REGFRM1_SSR_PB_SUBMIT')))
 driver.find_element_by_link_text('Finish Enrolling').click()         # TODO: migrate link_text to id
 
 print('Good luck and have fun!')
